8f9fe9aecd39468f56948d1ff7624b21536c7308
Andy Kweon <63220455+skwe902@users.noreply.github.com>
2021-12-15 15:06:43 +1300

    Initial Commit

---
+++ iOSBible/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSBible
+//
+//  Created by Dani D on 15/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSBible/iOSBibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSBibleApp.swift
+//  iOSBible
+//
+//  Created by Dani D on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSBibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
