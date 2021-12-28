a8d24e19bfef8e13414316591cdd2bda2dff95fd
Andy Kweon <63220455+skwe902@users.noreply.github.com>
2021-12-28 17:53:31 +1300

    Update ContactDevView.swift
    Finished about me page
    Todo: implement searchview

--- iOSBible/ContactDevView.swift
+++ iOSBible/ContactDevView.swift
@@ -9,15 +9,36 @@ struct ContactDevView: View {
     var body: some View {
         VStack{
             Image("photo")
-                .frame(width: 300, height: 300)
+                .resizable()
+                .frame(width: 250, height: 250)
                 .clipShape(Circle())
                 .shadow(radius: 10)
-                .overlay(Circle().stroke(Color.gray, lineWidth: 10))
+                .overlay(Circle().stroke(Color.black, lineWidth: 5))
+//            Text("Andy Kweon")
+//                .font(.largeTitle)
+//                .underline()
+//                .bold()
+//                .multilineTextAlignment(.center)
+            Text("About Me:")
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+                .padding()
+            Text("Hi! My name is Andy Kweon, a Computer Systems engineering student from University of Auckland, New Zealand. \n\nI love making apps and I also enjoy doing a bit of Machine Learning as well. \n\nIf you encounter any bugs, errors or would like to see any features added onto this app, visit my Github page and open a new issue. \n\nGod Bless You!")
+                .font(.headline)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .multilineTextAlignment(.leading)
+                .padding()
             Spacer()
-            Text("This app was created by Andy Kweon")
+            Link(destination: URL(string: "https://github.com/skwe902")!) {
+                VStack {
+                    Image(systemName: "paperplane")
+                        .font(.largeTitle)
+                    Text("Visit my Github!")
+                }
+            }
             Spacer()
         }
-        
     }
 }
 
