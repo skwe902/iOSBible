544a9148b84008f3c46325ef439cc0bd27de157d
Andy Kweon <63220455+skwe902@users.noreply.github.com>
2021-12-28 15:26:27 +1300

    added photo

--- iOSBible/ContactDevView.swift
+++ iOSBible/ContactDevView.swift
@@ -7,7 +7,16 @@ import SwiftUI
 
 struct ContactDevView: View {
     var body: some View {
-        Text("This app was created by Andy Kweon")
+        VStack{
+            Image("photo")
+                .frame(width: 300, height: 300)
+                .clipShape(Circle())
+                .shadow(radius: 10)
+                .overlay(Circle().stroke(Color.gray, lineWidth: 10))
+            Spacer()
+            Text("This app was created by Andy Kweon")
+            Spacer()
+        }
         
     }
 }
