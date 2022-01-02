12bf3c9beafff40c231462a7de2ad699c321dc23
Andy Kweon <63220455+skwe902@users.noreply.github.com>
2022-01-02 13:55:56 +1300

    Update TestamentView.swift

--- iOSBible/TestamentView.swift
+++ iOSBible/TestamentView.swift
@@ -16,23 +16,27 @@ struct TestamentView: View {
                     .font(.title)
                     .frame(maxWidth: .infinity, alignment: .center)
                     .padding()
+                    .listRowBackground(Color.yellow)
                 ForEach(oldList, id: \.self){ book in
                     NavigationLink(destination: ChapterListView(chapterName: book)){
                         Text(book)
                             .frame(maxWidth: .infinity, alignment: .center)
                     }
                 }
+                .listRowBackground(Color.yellow)
                 Text("신약 (New Testament)")
                     .padding()
                     .font(.title)
                     .frame(maxWidth: .infinity, alignment: .center)
                     .padding()
+                    .listRowBackground(Color.yellow)
                 ForEach(newList, id: \.self){ book in
                     NavigationLink(destination: ChapterListView(chapterName: book)){
                         Text(book)
                             .frame(maxWidth: .infinity, alignment: .center)
                     }
                 }
+                .listRowBackground(Color.yellow)
                 NavigationLink(destination: ContactDevView()){
                     Text("Contact Developer")
                         .padding()
@@ -42,6 +46,7 @@ struct TestamentView: View {
             }
             .navigationBarTitle("개역개정", displayMode: .inline)
         }
+        .listRowBackground(Color.yellow)
     }
 }
 
